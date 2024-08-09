8ef322c4704066c0c7248d6bddde04ae88e8c71c
Lindsey Neff <nefflindseyma12601@gmail.com>
2024-08-09 09:35:10 -0500

    changed the name of the CSS file and the import for testing; issue persist

--- src/pages/quiz/index.jsx
+++ src/pages/quiz/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Header from '@/components/Header';
-import styles from '../styles/Quiz.css';
+import styles from '../styles/Quiz.module.css';
 
 export default function Questions() {
 
