a5353c3a068215a795800ff1b6a788b4c5c1aa1d
Eyram Tsagli <eyramtsagli@outlook.com>
2024-08-08 21:15:07 -0500

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,25 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="footer">
-        <div></div>
-        <div> <a className="about" href='./about'>About</a></div>
-        <div><a className='contact' href='./contact'>Contact</a></div>
+  <footer className='footer'>
+    
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
 
+    <nav>
+    <ul className="nav justify-content-center">
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="about">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="contact">Contact Us</a>
+      </li>
+      {/* <li class="nav-item">
+        <a class="nav-link" href="follow">Follow Us</a>
+      </li>  */}
+    </ul>
+    </nav>
 
     <div className="container"> 
         <div className="line"></div>
